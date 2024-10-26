ebcc3d94c71b3778ad526546d0e1f60790ba46ae
angcsherpa01@gmail.com <angcsherpa01@gmail.com>
2024-10-25 21:13:54 -0400

    i finally figured out how to store the right date for the right habit with the help of data-habit-index and ternary operator.

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 class Habit {
-  constructor(name, goal) {
+  constructor(name, goal, dates) {
     this.name = name;
     this.goal = goal;
+    this.dates = dates;
   }
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  customDate();
+  displayTable();
+  addCheckBoxListener();
+});
 
 //localStorage.clear()
 let habitForm = document
@@ -15,36 +22,25 @@ let habitForm = document
 let habitInput = document.getElementById("habitInput");
 let habitGoal = document.getElementById("habitGoal");
 let addBtn = document.getElementById("addBtn");
-let tableBody = document.getElementById("tableBody");
 let pastHeader = document.getElementById("past");
 let presentHeader = document.getElementById("present");
 let futureHeader = document.getElementById("future");
 let checkBox = document.getElementById("checkBox");
+let today = new Date();
+let present = today.getDate();
+let past = present - 1;
+let future = present + 1;
 
-window.addEventListener("DOMContentLoaded", () => {
-  displayTable();
-  customDate();
-  addCheckBoxListener();
-});
+let tableBody = document.getElementById("tableBody");
 
 let habitData = JSON.parse(localStorage.getItem("habitData")) || [];
-
+console.log(habitData)
 addBtn.addEventListener("click", addHabit);
 function addHabit() {
-  let habit = new Habit(habitInput.value, habitGoal.value);
+  let habit = new Habit(habitInput.value, habitGoal.value, []);
   habitData.push(habit);
   localStorage.setItem("habitData", JSON.stringify(habitData));
   displayTable();
-}
-
-function customDate() {
-  let today = new Date();
-  let present = today.getDate();
-  let past = present - 1;
-  let future = present + 1;
-  pastHeader.textContent = past;
-  presentHeader.textContent = present;
-  futureHeader.textContent = future;
 }
 
 function displayTable() {
@@ -54,25 +50,52 @@ function displayTable() {
     let habit = habitData[n];
     row = `<tr> 
     <td> ${habit.name} </td>
-    <td> <input class="checkBox" type="checkbox"></td>
-    <td> <input class="checkBox" type="checkbox"></td>
-    <td> <input class="checkBox" type="checkbox"> </td>
+    <td> <input class="checkBox" data-habit-index= ${n} id="past" ${ habit.dates.includes(past) ? "checked" : ""   }  type="checkbox"></td>
+    <td> <input class="checkBox" data-habit-index= ${n} id="present" ${ habit.dates.includes(present) ? "checked" : ""   } type="checkbox"></td>
+    <td> <input class="checkBox" data-habit-index= ${n} id="future" ${ habit.dates.includes(future) ? "checked" : ""   } type="checkbox"> </td>
     <td> ${habit.goal} </td>
-     <tr> `;
+    `;
 
-    tableBody.innerHTML += row;
+  tableBody.innerHTML += row;
   }
 }
 
+ 
 function addCheckBoxListener() {
-  addHabit()
   let checkBoxes = document.querySelectorAll(".checkBox");
   checkBoxes.forEach((checkBox) => {
     checkBox.addEventListener("click", (event) => {
-      if (checkBox.checked) {
-        habit.dates = []
+      habitIndex = checkBox.getAttribute("data-habit-index");
+      switch (checkBox.id) {
+        case "past":
+          habit = habitData[habitIndex];
+          habit.dates.push(past);
+          break;
+        case "present":
+          habit = habitData[habitIndex];
+          habit.dates.push(present);
+          break;
+        case "future":
+          habit = habitData[habitIndex];
+          habit.dates.push(future);
+          break;
       }
-      console.log(habitData)
+      localStorage.setItem("habitData", JSON.stringify(habitData));
     });
   });
 }
+
+function customDate() {
+  pastHeader.textContent = past;
+  presentHeader.textContent = present;
+  futureHeader.textContent = future;
+}
+
+// console.log("working")
+// console.log("past");
+// console.log("HabitData", habitData)
+// habit = habitData[0]
+// console.log("Habit", habit)
+// console.log("Habit Dates", habit.dates)
+// habit.dates.push(past)
+// console.log("Habit Dates", habit.dates)
